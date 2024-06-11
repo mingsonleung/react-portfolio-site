c956fbe942f8cc967a96bfc503757a4f4f580bbd
mingsonleung <83376393+mingsonleung@users.noreply.github.com>
2024-06-11 01:04:24 -0700

    Added certifications to Education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import EducationItem from "./EducationItem";
 
-const data = [
+const data_education = [
   {
     title: "University of California - San Diego",
     location: "La Jolla, CA",
@@ -11,14 +11,17 @@ const data = [
     gpa: "3.7/4.0",
     awards: "Regent Scholarship (Merit-based)",
   },
+];
+
+const data_certs = [
+  // Reusing EducationItem component to handle this case
   {
-    title: "Franklin High School",
-    location: "Elk Grove, CA",
-    year: "August 2016 - May 2020",
-    major: "",
-    minor: "",
-    gpa: "",
-    awards: "",
+    title: "CompTIA A+, Network+, Security+",
+    year: "Active: May 2024 - May 2027",
+  },
+  {
+    title: "AWS Certified Cloud Practitioner",
+    year: "Active: Feb 2024 - Feb 2027",
   },
 ];
 
@@ -26,12 +29,17 @@ const Education = () => {
   return (
     <div
       id="education"
-      className="max-w-[1040px] min-h-screen m-auto md:pl-20 p-4 py-16"
-    >
+      className="max-w-[1040px] min-h-screen m-auto md:pl-20 p-4 py-16">
       <h1 className="mb-4 text-4xl font-bold text-center text-[#001b5e]">
         Education
       </h1>
-      {data.map((item, idx) => (
+      {data_education.map((item, idx) => (
+        <EducationItem key={idx} {...item} />
+      ))}
+      <h1 className="mb-4 text-4xl font-bold text-center text-[#001b5e]">
+        Certifications
+      </h1>
+      {data_certs.map((item, idx) => (
         <EducationItem key={idx} {...item} />
       ))}
     </div>
